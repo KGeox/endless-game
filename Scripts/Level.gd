e3f9a3514a6ec1d98a81d60db96f86914826048f
Georges <georgeskuetche93@gmail.com>
2026-08-15 13:28:48 +0100

    updating the background

--- Scripts/Level.gd
+++ Scripts/Level.gd
@@ -1,7 +1,7 @@
 extends Node
 
 const PLAYER_START_POS := Vector2i(88, 470)
-const CAM_START_POS := Vector2i(577, 326)
+#const CAM_START_POS := Vector2i(577, 326)
 
 var speed : float
 const START_SPEED : float = 10
@@ -11,16 +11,17 @@ func _ready():
 	new_game()
 	
 func new_game():
-	$Player.position = PLAYER_START_POS
-	$Player.velocity = Vector2i(0, 0)
-	$Camera2D.position =CAM_START_POS
-	$Ground.position = Vector2i(594,610)
+	pass
+	#$Player2.position = PLAYER_START_POS
+	#$Player2.velocity = Vector2i(0, 0)
+	#$Camera2D.position =CAM_START_POS
+	#$Ground.position = Vector2i(594,6414)
 	
 func _process(delta):
 	speed = START_SPEED
 	
-	$Player.position.x +=speed
-	$Camera2D.position.x +=speed
+	#$Player.position.x +=speed
+	#$Camera2D.position.x +=speed
 	
 	
 	
